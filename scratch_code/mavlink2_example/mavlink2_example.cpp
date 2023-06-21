22996ad620b5cb3110e40c1b2c2c67a35bac0f52
Cameron Rose <cameroncrose72junk@gmail.com>
2023-06-21 19:24:47 -0400

    Updated mavlink example - works with SITL

--- scratch_code/mavlink2_example/mavlink2_example.cpp
+++ scratch_code/mavlink2_example/mavlink2_example.cpp
@@ -6,7 +6,7 @@
 #include <unistd.h>
 
 #define SERIAL_PORT "/dev/ttyTHS1"
-#define BAUD_RATE B921600
+#define BAUD_RATE B115200
 
 // Function prototypes
 void print_heartbeat(mavlink_heartbeat_t heartbeat);
@@ -91,6 +91,21 @@ void print_command_ack(mavlink_command_ack_t command_ack) {
     printf("\tResult: %u\n", command_ack.result);
 }
 
+void print_param_request_read(const mavlink_param_request_read_t& param_request_read) {
+    printf("Param Request Read:\n");
+    printf("\tTarget System: %u\n", param_request_read.target_system);
+    printf("\tTarget Component: %u\n", param_request_read.target_component);
+    printf("\tParameter ID: %s\n", param_request_read.param_id);
+}
+
+void print_request_data_stream(const mavlink_request_data_stream_t& request_data_stream) {
+    printf("REQUEST_DATA_STREAM message:\n");
+    printf("\tTarget System: %u\n", request_data_stream.target_system);
+    printf("\tTarget Component: %u\n", request_data_stream.target_component);
+    printf("\tStream ID: %u\n", request_data_stream.req_stream_id);
+    printf("\tMessage Rate: %u\n", request_data_stream.req_message_rate);
+    printf("\tStart/Stop: %u\n", request_data_stream.start_stop);
+}
 
 int main() {
     // Open the serial port for reading and writing
@@ -116,25 +131,41 @@ int main() {
 
     // Initialize the Mavlink status
     mavlink_status_t status = {};
+
+    // define system and component ID for companion computer
+    uint8_t SENDER_SYS_ID = 0;
+    uint8_t SENDER_COMP_ID = 0;
 	
     // Subscribe to HEARTBEAT message
-    mavlink_msg_heartbeat_pack(0, MAV_COMP_ID_ALL, &msg, MAV_TYPE_ONBOARD_CONTROLLER, MAV_AUTOPILOT_INVALID, 0, 0, MAV_STATE_STANDBY);
+    mavlink_msg_heartbeat_pack(SENDER_SYS_ID, SENDER_COMP_ID, &msg, MAV_TYPE_ONBOARD_CONTROLLER, MAV_AUTOPILOT_INVALID, 0, 0, MAV_STATE_STANDBY);
+
+    uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
 
 	// Request all message streams at a rate of X Hz
 	// system_id, component_id, msg, target_system. target_component, req_stream_id, req_message_rate, start_stop
 	// mavlink_msg_request_data_stream_pack(0, MAV_COMP_ID_ALL, &msg, 0, 0, MAV_DATA_STREAM_ALL, 50, 1);
 
-	// Set the message rate for MAVLINK message to X Hz
-	// system_id, component_id, msg pointer, target_system, target_component, command, 0, param1, param2, param3, param4, param5, param6, param7
-	mavlink_msg_command_long_pack(0, MAV_COMP_ID_ALL, &msg, 0, 0, MAV_CMD_SET_MESSAGE_INTERVAL, 0, MAV_DATA_STREAM_ALL, 100, 0, 0, 0, 0, 0);
-	
+    // Set the message rate for MAVLINK message to X Hz
+    // system_id, component_id, msg pointer, target_system, target_component, command, 0, param1, param2, param3, param4, param5, param6, param7
+    mavlink_msg_command_long_pack(SENDER_SYS_ID, SENDER_COMP_ID, &msg, 1, 1, MAV_CMD_SET_MESSAGE_INTERVAL, 0, MAVLINK_MSG_ID_RAW_IMU, 10000, 0, 0, 0, 0, 0);
+
+    len = len + mavlink_msg_to_send_buffer(&buffer[len], &msg);
+
+    // Request specific MAVLINK message
+    mavlink_msg_command_long_pack(SENDER_SYS_ID, SENDER_COMP_ID, &msg, 1, 1, MAV_CMD_REQUEST_MESSAGE, 0, MAVLINK_MSG_ID_RAW_IMU, 0, 0, 0, 0, 0, 0);
+
+    len = len + mavlink_msg_to_send_buffer(&buffer[len], &msg);
+    
     // Set the flight mode
 	mavlink_msg_command_long_pack(0, MAV_COMP_ID_ALL, &msg, 0, 0, MAV_CMD_DO_SET_MODE, 0, MAV_MODE_GUIDED_ARMED, 0, 0, 0, 0, 0, 0);
+    
+    len = len + mavlink_msg_to_send_buffer(&buffer[len], &msg);
     
     // Command to ARM the drone
 	mavlink_msg_command_long_pack(0, MAV_COMP_ID_ALL, &msg, 0, 0, MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 1, 0, 0, 0, 0, 0);
 	
-    uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+    len = len + mavlink_msg_to_send_buffer(&buffer[len], &msg);
+    
 
     // Send message to flight controller
     ssize_t n = write(serial_port, buffer, len);
@@ -191,6 +222,16 @@ int main() {
 					mavlink_msg_command_ack_decode(&msg, &command_ack);
 					print_command_ack(command_ack);
 					break;
+                case MAVLINK_MSG_ID_PARAM_REQUEST_READ:
+                    mavlink_param_request_read_t param_request_read;
+                    mavlink_msg_param_request_read_decode(&msg, &param_request_read);
+                    print_param_request_read(param_request_read);
+                    break;
+                case MAVLINK_MSG_ID_REQUEST_DATA_STREAM:
+                    mavlink_request_data_stream_t request_data_stream;
+                    mavlink_msg_request_data_stream_decode(&msg, &request_data_stream);
+                    print_request_data_stream(request_data_stream);
+                    break;
 	        default:
 		    	std::cout << "Received message with ID " << (int)msg.msgid << std::endl;
             }
